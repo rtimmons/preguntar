c53a7364ca77a70968e8161f04759de84a7426fd
Ryan Timmons <rtimmons@betterment.com>
2016-10-09 10:48:48 -0400

    only the best

--- quizr.js
+++ quizr.js
@@ -1,48 +1,50 @@
-var verbs = {
-  talk: {
-    esp: 'hablar'
-  },
-  study: {
-    esp: 'estudiar',
-  },
-};
-
-var verbTypes = {
-  regar: {
-    present: {
-      first: 'o',
-      secondInf: 'as',
-      third: 'a'
+var config = {
+  verbs: {
+    talk: {
+      esp: 'hablar',
+    },
+    study: {
+      esp: 'estudiar',
     },
   },
-};
 
-var persons = {
-  first: {
-    esp: '',
-    eng: 'I',
+  types: {
+    regar: {
+      present: {
+        first: 'o',
+        secondInf: 'as',
+        third: 'a',
+      },
+    },
   },
-  secondInf: {
-    esp: 'tú',
-    eng: 'You (friendly)'
+
+  persons: {
+    first: {
+      esp: '',
+      eng: 'I',
+    },
+    secondInf: {
+      esp: 'tú',
+      eng: 'You (friendly)',
+    },
+    third: {
+      esp: 'él/ella',
+      eng: 'He/she',
+    },
   },
-  third: {
-    esp: 'él/ella',
-    eng: 'He/she'
-  }
 };
 
 var EXTRACT_ROOT = /^(.*?)(..)$/g;
 
 function questionFor(verbKey, person, tense) {
-  var verb = verbs[verbKey];
+  var verb = config.verbs[verbKey];
 
   // e.g. 'regar' for regular ar verbs
   var typeKey = verb.type || verb.esp.replace(EXTRACT_ROOT,'reg$2')
   // hablar => habl
   var root    = verb.root || verb.esp.replace(EXTRACT_ROOT,'$1');
 
-  var typ = verbTypes[typeKey];
+  var typ = config.types[typeKey];
   
   var suffix = typ[tense][person];
   return root + suffix;
@@ -53,44 +55,8 @@ $(() => {
   $('body').append($('<div>').html(q + ''));
 });
 
-var dimensions = [
-  [
-    'I',
-    'You (inf)',
-    'He/she',
-    'We',
-    'You (plural)',
-    'They (plural)',
-  ],
-  [
-    '(present)',
-    '(past)',
-    'will',
-  ],
-  [
-    'talk',
-    'work',
-    'study',
-    'drink',
-    'walk',
-    'travel',
-  ]
-];
-
 var rand = function(array) {
   var r = Math.round(Math.random() * (array.length-1));
   var out = array[r];
   return out;
 };
-
-$(() => {
-  $('#quizr').click(() => {
-    var ix = _.map(dimensions, d => rand(d));
-    var q = _.map(dimensions, d => {
-      var out = rand(d);
-      return out;
-    }).join(' ');
-    $('#quizr').html(q);
-  })
-  .click();
-});
