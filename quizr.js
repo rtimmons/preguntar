d7119cea199facd47d9bb82834c5927f48c2a32b
Ryan Timmons <rtimmons@betterment.com>
2016-10-09 18:55:47 -0400

    stemmer concept

--- quizr.js
+++ quizr.js
@@ -9,23 +9,27 @@ var EXTRACT_ROOT = /^(.*?)(..)$/g;
 function questionAndAnswerFor(verbKey, person, tense) {
   var verb = config.verbs[verbKey];
 
-  var engConjugation =
+  var question =
+    config.persons[person].eng + ' ' +
     (config.tenses[tense].eng || (x => x))(verb.eng || verbKey);
 
   var type = _.find(_.collect(
     verb.type || [verb.esp.replace(EXTRACT_ROOT,'reg$2')], 
     t => config.types[t]
   ), typ => typ[tense]);
-
   var espSuffix = type[tense][person];
 
-  var espRoot =
-    verb.stems && verb.stems[tense] ||
-    config.tenses[tense].esp && config.tenses[tense].esp(verb) ||
-    verb.esp.replace(EXTRACT_ROOT,'$1');
+  var stemmer =
+    // use per-verb stem for given tense
+    (verb.stems && verb.stems[tense] && (() => verb.stems[tense]) ) ||
+    // else use stemmer for tense
+    config.tenses[tense].esp ||
+    // else remove last 2 (suffix)
+    (v => v.esp.replace(EXTRACT_ROOT,'$1'));
+  var espRoot = stemmer(verb);
 
   return {
-    question: config.persons[person].eng + ' ' + engConjugation,
+    question: question,
     answer:   espRoot + espSuffix,
   };
 }
