4bb229f4f4d369850408a8887ae554d05be56b99
Ryan Timmons <rtimmons@betterment.com>
2016-10-09 16:55:25 -0400

    wip to refactor questionAndAnswerFor - tests pass

--- quizr.js
+++ quizr.js
@@ -6,51 +6,35 @@ if (typeof require === 'function') {
 
 var EXTRACT_ROOT = /^(.*?)(..)$/g;
 
-function questionFor(verbKey, person, tense) {
+function questionAndAnswerFor(verbKey, person, tense) {
   var verb = config.verbs[verbKey];
 
   var engVerb = verb.eng || verbKey;
-  var rootCtx = _.defaults(verb, {
-    engVerb: engVerb,
-  });
-  var tensed = (config.tenses[tense].eng || (ctx => ctx.engVerb))(rootCtx);
-
   var engPerson = config.persons[person].eng;
-
-  return engPerson + ' ' + tensed;
-}
-
-function answerFor(verbKey, person, tense) {
-  var verb = config.verbs[verbKey];
 
   // e.g. 'regar' for regular ar verbs
   var typeKey = verb.type || verb.esp.replace(EXTRACT_ROOT,'reg$2')
-  // hablar => habl
-  var stem    = verb.stem || verb.esp.replace(EXTRACT_ROOT,'$1');
 
   var typ = config.types[typeKey];
+  var espSuffix = typ[tense][person];
 
   if (!typ || !typ[tense]) {
     return `${typeKey} ${tense} not configured`;
   }
-  
+
+  // hablar => habl
+  var stem = verb.stem || verb.esp.replace(EXTRACT_ROOT,'$1');
+
   var rootCtx = _.defaults(verb, {
+    engVerb: engVerb,
     stem: stem,
   });
+  var engConjugation = (config.tenses[tense].eng || (ctx => ctx.engVerb))(rootCtx);
   var root = (config.tenses[tense].esp || (ctx => ctx.stem))(rootCtx);
 
-  var suffix = typ[tense][person];
-  return root + suffix;
-}
-
-
-function questionAndAnswerFor(verbKey, person, tense) {
-  var q = questionFor(verbKey,person,tense);
-  var a = answerFor(verbKey,person,tense);
-
   return {
-    question: q,
-    answer:   a,
+    question: engPerson + ' ' + engConjugation,
+    answer:   root + espSuffix,
   };
 }
 
