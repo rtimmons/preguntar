ad8dec0fcda02d95a85e1d42a938652bb59e013b
Ryan Timmons <rtimmons@betterment.com>
2016-10-09 19:57:49 -0400

    handle 'to go' properly

--- app/config.js
+++ app/config.js
@@ -49,7 +49,67 @@ var config = {
         future: 'tendr',
         past:   'tuv',
       },
-    }
+    },
+
+
+    go: {
+      esp: {
+        infinitive: 'ir',
+        present: {
+          first:      'voy',
+          secondInf:  'vas',
+          third:      'va',
+          firstPlural:'vamos',
+          secondForm: 'vais',
+          thirdPlural:'van',
+        },
+        past: {
+          first:      'fui',
+          secondInf:  'fuiste',
+          third:      'fue',
+          firstPlural:'fuistemos',
+          secondForm: 'fuisteis',
+          thirdPlural:'fueran',
+        },
+        future: {
+          // TODO: is this right?
+          first:      'iré',
+          secondInf:  'irás',
+          third:      'irá',
+          firstPlural:'iremos',
+          secondForm: 'iréis',
+          thirdPlural:'iran',
+        },
+      },
+      eng: {
+        infinitive: 'to go',
+        present: {
+          first:      'go',
+          secondInf:  'go',
+          third:      'goes',
+          firstPlural:'go',
+          secondForm: 'go',
+          thirdPlural:'go',
+        },
+        past: {
+          first:      'went',
+          secondInf:  'went',
+          third:      'went',
+          firstPlural:'went',
+          secondForm: 'went',
+          thirdPlural:'went',
+        },
+        future: {
+          first:      'will go',
+          secondInf:  'will go',
+          third:      'will go',
+          firstPlural:'will go',
+          secondForm: 'will go',
+          thirdPlural:'will go',
+        },
+      }
+    },
+
   },
 
   persons: {
--- app/quizr.js
+++ app/quizr.js
@@ -11,26 +11,36 @@ function questionAndAnswerFor(verbKey, person, tense) {
 
   var question =
     config.persons[person].eng + ' ' +
-    (config.tenses[tense].eng || (x => x))(verb.eng || verbKey);
+    ((verb.eng
+      && typeof(verb.eng) !== 'string'
+      && verb.eng[tense]
+      && verb.eng[tense][person]) || (config.tenses[tense].eng || (x => x))(verb.eng || verbKey));
 
-  var type = _.find(_.collect(
-    verb.type || [verb.esp.replace(EXTRACT_ROOT,'reg$2')], 
-    t => config.types[t]
-  ), typ => typ[tense]);
-  var espSuffix = type[tense][person];
+  function computeAnswer() {
+    var type = _.find(_.collect(
+      verb.type || [verb.esp.replace(EXTRACT_ROOT,'reg$2')], 
+      t => config.types[t]
+    ), typ => typ[tense]);
+    var espSuffix = type[tense][person];
 
-  var stemmer =
-    // use per-verb stem for given tense
-    (verb.stems && verb.stems[tense] && (() => verb.stems[tense]) ) ||
-    // else use stemmer for tense
-    config.tenses[tense].esp ||
-    // else remove last 2 (suffix)
-    (v => v.esp.replace(EXTRACT_ROOT,'$1'));
-  var espRoot = stemmer(verb);
+    var stemmer =
+      // use per-verb stem for given tense
+      (verb.stems && verb.stems[tense] && (() => verb.stems[tense]) ) ||
+      // else use stemmer for tense
+      config.tenses[tense].esp ||
+      // else remove last 2 (suffix)
+      (v => v.esp.replace(EXTRACT_ROOT,'$1'));
+    var espRoot = stemmer(verb);
+
+    return espRoot + espSuffix;
+  }
+
+  var answer = verb.esp && verb.esp[tense] && verb.esp[tense][person] ||
+    computeAnswer();
 
   return {
     question: question,
-    answer:   espRoot + espSuffix,
+    answer:   answer,
   };
 }
 
