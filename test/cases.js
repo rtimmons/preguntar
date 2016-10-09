8380715db3aed2eb573eaf84be7334cce4c3fa94
Ryan Timmons <rtimmons@betterment.com>
2016-10-09 16:13:39 -0400

    yaml tests ftw

--- test/cases.js
+++ test/cases.js
@@ -9,6 +9,20 @@ var _ = require('underscore');
 var quizr = require('../quizr.js');
 
 describe('conjugation',() => {
+  var doc = yaml.safeLoad(fs.readFileSync('./test/cases.yaml', 'utf8'));
+
+  var only = _.findWhere(doc.cases, { only: true });
+
+  (only ? [only] : doc.cases).forEach((c) => {
+    it('given ' + c.given, () => {
+      var given = c.given.split(/\s+/);
+      var actual = quizr.questionAndAnswerFor.apply(null, given);
+      expect(actual).to.deep.equal({
+        question: c.question,
+        answer:   c.answer,
+      });
+    });
+  });
   it('passes', () => {
     expect(quizr.questionAndAnswerFor('talk', 'first', 'present')).to.deep.equal({
       question: 'I talk',
